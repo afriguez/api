ed797d2abd7516f481865393d5375b0490fbfc0d
Fer L. <afriguez@gmail.com>
2024-01-01 20:41:27 -0500

    feat: initial commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Api.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :api,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Api.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:plug_cowboy, "~> 2.6.1"},
+      {:poison, "~> 5.0"}
+    ]
+  end
+end
