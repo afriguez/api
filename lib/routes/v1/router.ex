eefa582efb473614acbd2f3b3e728c900d71d50a
Fer L. <afriguez@gmail.com>
2024-01-04 09:33:54 -0500

    feat: use games client to query games & get covers

--- lib/routes/v1/router.ex
+++ lib/routes/v1/router.ex
@@ -1,12 +1,22 @@
 defmodule Api.Routes.V1 do
   use Plug.Router
 
+  alias Api.Clients.Games
+
   plug(:match)
   plug(:dispatch)
 
   get "/game/:query" do
-    # %Plug.Conn{params: %{"query" => query}} = conn
-    send_resp(conn, 200, "OK")
+    %Plug.Conn{params: %{"query" => query}} = conn
+
+    cover =
+      query
+      |> Games.search_game("cover")
+      |> Games.get_cover()
+
+    url = String.replace(cover["url"], "t_thumb", "t_cover_big")
+    data = %{cover | "url" => url}
+    send_resp(conn, 200, Poison.encode!(%{success: true, data: data}))
   end
 
   match _ do
