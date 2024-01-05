314d150a90d6a392a0f9c98f3996f10f3e4b49ed
Fer L. <afriguez@gmail.com>
2024-01-05 07:34:33 -0500

    test: update api_test.exs

--- test/api_test.exs
+++ test/api_test.exs
@@ -4,13 +4,51 @@ defmodule ApiTest.Router do
 
   @opts Api.Router.init([])
 
-  test "Return OK" do
-    conn = conn(:get, "/")
+  describe "Main Router" do
+    test "Success when requesting /" do
+      conn = conn(:get, "/")
 
-    conn = Api.Router.call(conn, @opts)
+      conn = Api.Router.call(conn, @opts)
+      body = Poison.decode!(conn.resp_body)
 
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert conn.resp_body == "OK"
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert body["success"]
+    end
+  end
+
+  describe "Games Router" do
+    test "Success when requesting a cover" do
+      conn = conn(:get, "/v1/games/cover/genshin%20impact")
+
+      conn = Api.Router.call(conn, @opts)
+      body = Poison.decode!(conn.resp_body)
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert body["success"]
+    end
+
+    test "Success when requesting a thumbnail" do
+      conn = conn(:get, "/v1/games/thumbnail/genshin%20impact")
+
+      conn = Api.Router.call(conn, @opts)
+      body = Poison.decode!(conn.resp_body)
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert body["success"]
+    end
+
+    test "Failure when requesting a cover" do
+      conn = conn(:get, "/v1/games/cover/genshinimpact")
+
+      conn = Api.Router.call(conn, @opts)
+      body = Poison.decode!(conn.resp_body)
+
+      assert conn.state == :sent
+      assert conn.status == 404
+      assert body["success"] == false
+    end
   end
 end
