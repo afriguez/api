7fb3b7b342f9abc178c11d3f2f1772fee38f1bb5
Fer L. <afriguez@gmail.com>
2024-01-05 07:17:42 -0500

    feat: add thumbnail route for games

--- lib/routes/v1/router.ex
+++ lib/routes/v1/router.ex
@@ -1,22 +1,38 @@
 defmodule Api.Routes.V1 do
   use Plug.Router
 
-  alias Api.Clients.Games
+  alias Api.Clients.IGDB
 
   plug(:match)
   plug(:dispatch)
 
-  get "/game/:query" do
+  get "/games/cover/:query" do
     %Plug.Conn{params: %{"query" => query}} = conn
 
-    cover =
-      query
-      |> Games.search_game("cover")
-      |> Games.get_cover()
+    case IGDB.search_game(query, "cover") do
+      {:ok, game} ->
+        cover = IGDB.get_cover!(game)
+        url = String.replace(cover["url"], "t_thumb", "t_cover_big")
+        data = %{"url" => url}
+        send_resp(conn, 200, Poison.encode!(%{success: true, data: data}))
 
-    url = String.replace(cover["url"], "t_thumb", "t_cover_big")
-    data = %{cover | "url" => url}
-    send_resp(conn, 200, Poison.encode!(%{success: true, data: data}))
+      {:error, reason} ->
+        send_resp(conn, 404, reason)
+    end
+  end
+
+  get "/games/thumbnail/:query" do
+    %Plug.Conn{params: %{"query" => query}} = conn
+
+    case IGDB.search_game(query, "cover") do
+      {:ok, game} ->
+        cover = IGDB.get_cover!(game)
+        data = %{"url" => cover["url"]}
+        send_resp(conn, 200, Poison.encode!(%{success: true, data: data}))
+
+      {:error, reason} ->
+        send_resp(conn, 404, reason)
+    end
   end
 
   match _ do
